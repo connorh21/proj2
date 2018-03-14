861936446cd48928d3a74cb7e1f8dfa91986b314
Connor <connor.hamilton@bobcats.gcsu.edu>
2018-03-14 13:37:27 -0400

    transfering files from proj_1 into our proj_2

---
+++ private/db_config.php
@@ -0,0 +1,11 @@
+<?php
+
+// database constants
+// you need to change these values to match your Database
+// these values need a ""
+define("DB_SERVER", "localhost");
+define("DB_USER", "connorh0_pro1");
+define("DB_PASS", "Casper2015");
+define("DB_NAME", "connorh0_pro1");
+
+?>
